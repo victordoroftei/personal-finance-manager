cb3299486977a13c44651744883468e37a60e2e9
Vic <victordoro78@gmail.com>
2023-03-04 14:46:46 +0200

    Added router and login screen

---
+++ src/app/components/login/auth.guard.ts
@@ -0,0 +1,28 @@
+import {Injectable} from "@angular/core";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {LoginService} from "../../services/login-service";
+import {map, Observable, take} from "rxjs";
+import jwtDecode from "jwt-decode";
+import {JWTPayload} from "./login.component";
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+
+  constructor(private loginService: LoginService, private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.loginService.loggedUser.pipe(take(1), map(user => {
+      const isAuth = !!user;
+
+      if (isAuth) {
+        const payload = jwtDecode(user.token!) as JWTPayload;
+
+        if (payload.scope === "ROLE_ADMIN") {
+          return true;
+        }
+      }
+      return this.router.createUrlTree(["/login"]);
+    }));
+  }
+}
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import {LoginService} from "../../services/login-service";
+import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import jwtDecode from "jwt-decode";
+
+export interface JWTPayload {
+  exp: number;
+  iat: number;
+  iss: string;
+  scope: string;
+  sub: string;
+}
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private loginService: LoginService, private router: Router) {
+
+  }
+
+  loginForm = new FormGroup({
+    emailAddress: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  showEmailAddressError = false;
+  showPasswordError = false;
+  isLoading = false;
+  responseError = null;
+
+  get emailAddress() {
+    return this.loginForm.get('input-email-address');
+  }
+
+  get password() {
+    return this.loginForm.get('input-password');
+  }
+
+  onSubmit(form: FormGroup) {
+    const email = form.value.emailAddress;
+    const password = form.value.password;
+
+    this.showEmailAddressError = false;
+    this.showPasswordError = false;
+    this.responseError = null;
+
+    if (email == null || email == "") {
+      this.showEmailAddressError = true;
+    }
+
+    if (password == null || password == "") {
+      this.showPasswordError = true;
+    }
+
+    if (!this.showEmailAddressError && !this.showPasswordError) {
+      this.isLoading = true;
+      this.loginService.login(email, password).subscribe(() => {
+          this.isLoading = false;
+          this.responseError = null;
+          const token = localStorage.getItem("token");
+          const payload = jwtDecode(token!) as JWTPayload;
+
+          if (payload.scope === "ROLE_ADMIN") {
+            this.router.navigate(["/main-view/main-page"]);
+          }
+        },
+        error => {
+          this.responseError = error.error;
+          this.isLoading = false;
+        });
+      form.reset();
+    }
+  }
+
+  createAccount() {
+    this.router.navigate(["/register"]);
+  }
+
+}
